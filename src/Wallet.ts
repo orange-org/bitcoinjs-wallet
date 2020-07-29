b258125071604b9c5732b78d0009ef8a5ab4cac3
M.K. Safi <msafi@msafi.com>
2020-07-29 10:47:03 -0700

    Add a couple of tests

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,3 +1,68 @@
+import * as bip39 from 'bip39';
+import * as bip32 from 'bip32';
+import * as bjs from 'bitcoinjs-lib';
+
+type WalletConfigNoHdNode = {
+  derivationPath?: string;
+  network?: bjs.Network;
+};
+
+type WalletConfig = WalletConfigNoHdNode & {
+  hdNode: bip32.BIP32Interface;
+};
+
 export class Wallet {
-  static generateMnemonic = () => true;
+  hdNode: bip32.BIP32Interface;
+
+  derivationPath: string;
+
+  network: bjs.Network;
+
+  addresses: string[] = [];
+
+  constructor(config: WalletConfig) {
+    this.hdNode = config.hdNode;
+    this.derivationPath = config.derivationPath || 'm/84/0/0';
+    this.network = config.network || bjs.networks.bitcoin;
+  }
+
+  static async fromMnemonic(
+    mnemonic: string,
+    config: WalletConfigNoHdNode = {},
+  ) {
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    const hdNode = bip32.fromSeed(seed, config.network);
+
+    return new Wallet({ ...config, hdNode });
+  }
+
+  static fromMasterPublicKey(
+    masterPublicKey: string,
+    config: WalletConfigNoHdNode = {},
+  ) {
+    const hdNode = bip32.fromBase58(masterPublicKey, config.network);
+
+    return new Wallet({ ...config, hdNode });
+  }
+
+  /**
+   * Makes network calls to 3PBP to look up the balances of addresses in this wallet.
+   */
+  async populateBalance() {
+    throw new Error('Not implemented');
+  }
+
+  generateAddress() {
+    const nextIndex = this.addresses.length;
+    const childPublicKey = this.hdNode.derivePath(
+      `${this.derivationPath}/0/${nextIndex}`,
+    ).publicKey;
+    const { address } = bjs.payments.p2wpkh({ pubkey: childPublicKey });
+
+    if (address == undefined) {
+      throw new Error('Could not create address');
+    }
+
+    this.addresses.push(address);
+  }
 }
--- tests/basics.test.ts
+++ tests/basics.test.ts
@@ -1,9 +1,26 @@
 import { Wallet } from 'src/Wallet';
 
-describe('Wallet basics', () => {
-  it('has a static function that generates a mnemonic', () => {
-    const mnemonic = Wallet.generateMnemonic();
+const mnemonic =
+  'verify never climb able arch joy eight stand razor nuclear parade park';
 
-    expect(mnemonic).toBe(true);
+describe('Wallet basics', () => {
+  it('can be initialized with a BIP32 node', async () => {
+    const wallet = await Wallet.fromMnemonic(mnemonic);
+
+    expect(wallet.hdNode.chainCode).toBeDefined();
+  });
+
+  it('can generate receiving addresses', async () => {
+    const wallet = await Wallet.fromMnemonic(mnemonic);
+
+    wallet.generateAddress();
+    wallet.generateAddress();
+
+    expect(wallet.addresses[0]).toEqual(
+      'bc1q0us48prke8q5duz0rm45z9aax3aztsd6xde6l9',
+    );
+    expect(wallet.addresses[1]).toEqual(
+      'bc1q87acgzdfkcsgkkrmq69cx3p3ddkk57e36u0dc0',
+    );
   });
 });
